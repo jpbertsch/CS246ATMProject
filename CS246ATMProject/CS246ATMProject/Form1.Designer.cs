d88d0f1014a81b2efce268a16defafbb7bee0b65
jpbertsch <jpbertsch91@gmail.com>
2015-02-05 13:20:59 -0800

    Added Withdrawal Sequence Diagram
    Added a basic sequence diagram for the withdrawal use case

--- CS246ATMProject/CS246ATMProject/Form1.Designer.cs
+++ CS246ATMProject/CS246ATMProject/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CS246ATMProject
 {
-    partial class Form1
+    partial class PrimaryWindowForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SwipeButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SwipeButton
+            // 
+            this.SwipeButton.Location = new System.Drawing.Point(12, 12);
+            this.SwipeButton.Name = "SwipeButton";
+            this.SwipeButton.Size = new System.Drawing.Size(112, 32);
+            this.SwipeButton.TabIndex = 0;
+            this.SwipeButton.Text = "Log In / Swipe Card";
+            this.SwipeButton.UseVisualStyleBackColor = true;
+            this.SwipeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // PrimaryWindowForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(488, 579);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SwipeButton);
+            this.Name = "PrimaryWindowForm";
+            this.Text = "ATM Interface";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SwipeButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CS246ATMProject/CS246ATMProject/Program.cs
+++ CS246ATMProject/CS246ATMProject/Program.cs
@@ -16,7 +16,7 @@ namespace CS246ATMProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PrimaryWindowForm());
         }
     }
 }
